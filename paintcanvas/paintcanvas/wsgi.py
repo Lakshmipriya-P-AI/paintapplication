c82badbfff32c3c49c6758c21681b556beed7749
Lakshmipriya-P-AI <plakshmipriya82@gmail.com>
2022-01-26 13:34:44 +0530

    completed

---
+++ paintcanvas/paintcanvas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for paintcanvas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paintcanvas.settings')
+
+application = get_wsgi_application()
